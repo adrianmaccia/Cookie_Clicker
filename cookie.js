d29229a7fdff39098ad411040e76b4ed4ac37970
Adrian Maccia <103082386@student.swin.edu.au>
2021-04-26 08:47:45 +1000

    upload 2

--- cookie.js
+++ cookie.js
@@ -1,12 +1,21 @@
-
-
-
-
-
 var cookiecount = 0;
+var autoClick = 0;
 
 function cookieClick(){
     cookiecount += 1;
     document.getElementById('counter').innerHTML = cookiecount;
+}
+
+
+function upgrade(){
+    if (cookiecount >= ((autoClick+1) * 10)) {  
+        cookiecount -= ((autoClick+1) * 10);   
+        autoClick += 1; 
+        document.getElementById('upgradeCost').innerHTML = "Purchase for " + ((autoClick+1) * 10) + " Cookies";
+        document.getElementById('upgradeAmount').innerHTML = "Clicks per second: " + autoClick;
+    }
+    else{
+        alert('You dont have enough cookies')
+    }
 }
 
