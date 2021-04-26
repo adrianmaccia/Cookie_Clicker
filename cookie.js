a1b503393495ab1b74cd4f3dc3db0ee68d9871e1
Adrian Maccia <103082386@student.swin.edu.au>
2021-04-26 10:47:01 +1000

    update 2.2

--- cookie.js
+++ cookie.js
@@ -1,17 +1,30 @@
 var cookiecount = 0;
 var autoClick = 0;
 
+var extraCook = 0;
+
 function cookieClick(){
     cookiecount += 1;
     document.getElementById('counter').innerHTML = cookiecount;
 }
 
-function upgrade(){
-    if (cookiecount >= ((autoClick+1) * 10)) {  
+function upgrade1(){
+    if (cookiecount >= ((autoClick+1) * 10)){  
         cookiecount -= ((autoClick+1) * 10);   
         autoClick += 1; 
-        document.getElementById('upgradeCost').innerHTML = "Purchase for " + ((autoClick+1) * 10) + " Cookies";
-        document.getElementById('upgradeAmount').innerHTML = "Clicks per second: " + autoClick;
+        document.getElementById('upgradeCost1').innerHTML = "Purchase for " + ((autoClick+1) * 10) + " Cookies";
+        document.getElementById('upgradeAmount1').innerHTML = "Clicks per second: " + autoClick;
+    }
+    else{
+        alert('You dont have enough cookies')
+    }
+}
+
+function upgrade2(){
+    if (cookiecount >= 100){  
+        cookiecount -= 100;   
+        extraCook += 1; 
+        document.getElementById('upgradeAmount2').innerHTML = "Clicks per second: " + extraCook;
     }
     else{
         alert('You dont have enough cookies')
@@ -20,5 +33,6 @@ function upgrade(){
 
 setInterval(function(){
     cookiecount += autoClick;
+    cookiecount += extraCook;
     document.getElementById('counter').innerHTML = cookiecount;
 },1000);
